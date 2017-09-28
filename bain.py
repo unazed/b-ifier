bbe797718a4a8febda7590b95fbce5eb4c254696
Unazed Spectaculum <leethax0r.coder@gmail.com>
2017-09-28 20:12:14 +0100

    :b:ade it :b:ork
    :b:ython 2 on:b:y

--- bain.py
+++ bain.py
@@ -1,19 +1,15 @@
 def bifify(bbbb):
-	bb =  {'!', "'", ',', ')', '(', 'm', 'o', 'y', '.', 'c', '?', ';', 't', 'd', 'a', 'h', ':', 'i', 'f', 'e', ' ', '/', 'k', 'u', 'l', 'r', 'g', '"', 'n', '\\'}
-  	bbb = b''
-  	for b in bbbb:
-    		if b not in bb:
-      			bbb += b":b:"
-      			continue
-    		bbb += b
-  	return bbb
+	bb =  {'!', "'", ',', ')', '(', 'm', 'o', 'y', '.', 'c', '?', ';', 't', 'd', 'a', 'h', ':', 'i', 'e', ' ', '/', 'k', 'u', 'l', 'r', 'g', '"', 'n', 
+'\\'}
+	bbb = b''
+	for b in bbbb:
+		if b not in bb:
+			bbb += b":b:"
+			continue
+		bbb += b
+	return bbb
 
 if __name__ == "__main__":
-	try:
-		raw_input
-		b = raw_input
-	except NameError:
-		raw_input = b = input
-	
+	b = b = raw_input
 	bb = b("b? ")
-	bbb = print = print(bifify(bb))  # bhis is inbenbiobal 
+	print(bifify(bb)) 
